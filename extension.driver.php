03f65084537d25071b0e9e7de495dbc0c1e1d9c0
animaux <ar@animaux.de>
2024-08-08 11:58:37 +0200

    PHP 8.3 fix, prevent unserialize-Error

--- extension.driver.php
+++ extension.driver.php
@@ -226,9 +226,74 @@
 
                 // split XML into an array of each line
                 $xml_lines = explode("\n", $xml);
-
+                
                 // output params are a serialised array on line 1
-                $output_params = @unserialize(trim($xml_lines[0]));
+                // if no params are included in the datasource, the datasource xsml root is displayed f. e. `<datasource cache-age='fresh' cache-expiration="-1">` we need to check if it’s actually a serialized string
+                // https://developer.wordpress.org/reference/functions/is_serialized/#Source_File
+                function is_serialized( $data, $strict = true ) {
+									// If it isn't a string, it isn't serialized.
+									if ( ! is_string( $data ) ) {
+										return false;
+									}
+									$data = trim( $data );
+									if ( 'N;' === $data ) {
+										return true;
+									}
+									if ( strlen( $data ) < 4 ) {
+										return false;
+									}
+									if ( ':' !== $data[1] ) {
+										return false;
+									}
+									if ( $strict ) {
+										$lastc = substr( $data, -1 );
+										if ( ';' !== $lastc && '}' !== $lastc ) {
+											return false;
+										}
+									} else {
+										$semicolon = strpos( $data, ';' );
+										$brace     = strpos( $data, '}' );
+										// Either ; or } must exist.
+										if ( false === $semicolon && false === $brace ) {
+											return false;
+										}
+										// But neither must be in the first X characters.
+										if ( false !== $semicolon && $semicolon < 3 ) {
+											return false;
+										}
+										if ( false !== $brace && $brace < 4 ) {
+											return false;
+										}
+									}
+									$token = $data[0];
+									switch ( $token ) {
+										case 's':
+											if ( $strict ) {
+												if ( '"' !== substr( $data, -2, 1 ) ) {
+													return false;
+												}
+											} elseif ( ! str_contains( $data, '"' ) ) {
+												return false;
+											}
+											// Or else fall through.
+										case 'a':
+										case 'O':
+										case 'E':
+											return (bool) preg_match( "/^{$token}:[0-9]+:/s", $data );
+										case 'b':
+										case 'i':
+										case 'd':
+											$end = $strict ? '$' : '';
+											return (bool) preg_match( "/^{$token}:[0-9.E+-]+;$end/", $data );
+									}
+									return false;
+								}
+                
+                /* only unserialize when string is actually serialized */
+                $output_params = '';
+                if (is_serialized($xml_lines[0])) {
+                  $output_params = @unserialize(trim($xml_lines[0]));
+                }                
 
                 // there are cached output parameters
                 if (is_array($output_params)) {
